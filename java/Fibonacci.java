cba79930c7e2a45dddeba6f3d9196c706091b48e
Connie <cleung334@gmail.com>
2017-02-14 05:56:54 +0800

    https://www.codewars.com/kata/the-millionth-fibonacci-kata/train/python
    Prepare shell

---
+++ java/Fibonacci.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class Fibonacci {
+
+  public static BigInteger fib(BigInteger n) {
+    // ...
+  }
+
+  public static void main(String[] args) {
+    
+  }
+
+}
