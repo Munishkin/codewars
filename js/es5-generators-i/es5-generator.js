379e1f7f3075c0b46efdd51d01328f01aa6a65d6
Connie <cleung334@gmail.com>
2017-03-06 23:19:25 +0800

    Complete fib sequence and factorial sequence

--- js/es5-generators-i/es5-generator.js
+++ js/es5-generators-i/es5-generator.js
@@ -69,33 +69,40 @@
 
 
 function SequenceGenerator(sequencer, args) {
-    this.sequencer = sequencer;
-    this.args = args;
+    this.func = sequencer(args);
 }
 
 SequenceGenerator.prototype.next = function() {
-  return this.sequencer(this.args).call(null, []);
+  return this.func.apply(null);
 }
 
 function generator(sequencer) {
-  //console.log (sequencer);
-  // remove the first mandatory argument
   let args = [].slice.call(arguments)
   args.splice(0, 1);
-  return new SequenceGenerator(sequencer, arguments);
+  return new SequenceGenerator(sequencer, args);
 }
 
 function dummySeq() {
-  return function() {
-    console.log ('in dummyseq');
-    return "dummy";
-  };
+  return () => { return "dummy"; };
 }
 
 function factorialSeq() {
+    let [counter, fact] = [-1, 1];
+    return () => {
+      ++counter;
+      if (counter <= 0) { fact = 1; }
+      else { fact = fact * counter; }
+      return fact;
+    }
 }
 
 function fibonacciSeq() {
+  let [prev, current] = [0, 1];
+  return () => {
+    let result = current;
+    [prev, current] = [current, prev + current];
+    return result;
+  }
 }
 
 function rangeSeq(start, step) {
@@ -107,70 +114,49 @@ function primeSeq() {
 function partialSumSeq() {
 }
 
-let gen = generator(dummySeq);
-console.log (gen.next());
-console.log (gen.next());
-console.log (gen.next());
-console.log (gen.next());
+let seq = generator(dummySeq);
+console.log (seq.next() === 'dummy');
+console.log(seq.next() === 'dummy');
+console.log(seq.next() === 'dummy');
 
+let seq2 = generator(factorialSeq);
+console.log(seq2.next() === 1); // 0! = 1
+console.log(seq2.next() === 1); // 1! = 1
+console.log(seq2.next() === 2); // 2! = 2
+console.log(seq2.next() === 6); // 3! = 6
+console.log(seq2.next() === 24); // 4! = 6
 
-Test.describe("ES5 Dummy generator", function() {
-  Test.it("Test dummy generator", function() {
-    var seq = generator(dummySeq);
-    Test.assertEquals(seq.next(), 'dummy');
-    Test.assertEquals(seq.next(), 'dummy');
-    Test.assertEquals(seq.next(), 'dummy');
-  });
-});
+let seq3 = generator(fibonacciSeq);
+console.log(seq3.next() === 1); // fib(0) = 1
+console.log(seq3.next() === 1); // fib(1) = 1
+console.log(seq3.next() === 2); // fib(2) = 2
+console.log(seq3.next() === 3); // fib(3) = 3
+console.log(seq3.next() === 5); // fib(4) = 5
+console.log(seq3.next() === 8); // fib(5) = 8
+console.log(seq3.next() === 13); // fib(6) = 13
 
-Test.describe("ES5 Simple Generators", function() {
+/*
+let seq4 = generator(rangeSeq, 5, 3); // 5,8,11,14,17
+console.log(seq4.next() === 5);
+console.log(seq4.next() === 8);
+console.log(seq4.next() === 11);
+console.log(seq4.next() === 14);
 
-  Test.it("Test factorial generator", function() {
-    var seq = generator(factorialSeq);
-    Test.assertEquals(seq.next(), 1); // 0! = 1
-    Test.assertEquals(seq.next(), 1); // 1! = 1
-    Test.assertEquals(seq.next(), 2); // 2! = 2
-    Test.assertEquals(seq.next(), 6); // 3! = 6
-    Test.assertEquals(seq.next(), 24); // 4! = 6
-  });
+var seq5 = generator(primeSeq);
+console.log(seq5.next() === 2);
+console.log(seq5.next() === 3);
+console.log(seq5.next() === 5);
+console.log(seq5.next() === 7);
+console.log(seq5.next() === 11);
+console.log(seq5.next() === 13);
+console.log(seq5.next() === 17);
+console.log(seq5.next() === 19);
 
-  Test.it("Test Fibonacci generator", function() {
-    var seq = generator(fibonacciSeq);
-    Test.assertEquals(seq.next(), 1); // fib(0) = 1
-    Test.assertEquals(seq.next(), 1); // fib(1) = 1
-    Test.assertEquals(seq.next(), 2); // fib(2) = 2
-    Test.assertEquals(seq.next(), 3); // fib(3) = 3
-    Test.assertEquals(seq.next(), 5); // fib(4) = 5
-    Test.assertEquals(seq.next(), 8); // fib(5) = 8
-    Test.assertEquals(seq.next(), 13); // fib(6) = 13
-  });
-
-  Test.it("Test Range generator", function() {
-    var seq = generator(rangeSeq, 5, 3); // 5,8,11,14,17
-    Test.assertEquals(seq.next(), 5);
-    Test.assertEquals(seq.next(), 8);
-    Test.assertEquals(seq.next(), 11);
-    Test.assertEquals(seq.next(), 14);
-  });
-
-  Test.it("Test Prime Numbers generator", function() {
-    var seq = generator(primeSeq);
-    Test.assertEquals(seq.next(), 2);
-    Test.assertEquals(seq.next(), 3);
-    Test.assertEquals(seq.next(), 5);
-    Test.assertEquals(seq.next(), 7);
-    Test.assertEquals(seq.next(), 11);
-    Test.assertEquals(seq.next(), 13);
-    Test.assertEquals(seq.next(), 17);
-    Test.assertEquals(seq.next(), 19);
-  });
-
-  Test.it("Test partial sum generator", function() {
-    var seq = generator(partialSumSeq, -1, 4, 2, 5);
-    Test.assertEquals(seq.next(), -1);
-    Test.assertEquals(seq.next(), 3);
-    Test.assertEquals(seq.next(), 5);
-    Test.assertEquals(seq.next(), 10); //End of sequence
-    Test.expectError('End of sequence error expected', seq.next);
-  });
-});
+let seq6 = generator(partialSumSeq, -1, 4, 2, 5);
+console.log(seq6.next() === -1);
+console.log(seq6.next() === 3);
+console.log(seq6.next() === 5);
+console.log(seq6.next() === 10); //End of sequence
+seq.next();  // expect exception thrown
+*/
+//Test.expectError('End of sequence error expected', seq.next);
