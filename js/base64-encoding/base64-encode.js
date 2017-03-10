7bc350dc95cf8dc4722a652aa6d5af79c4e2cfe4
Connie <cleung334@gmail.com>
2017-03-10 22:24:11 +0800

    skeleton codes

---
+++ js/base64-encoding/base64-encode.js
@@ -0,0 +1,9 @@
+//Extend the String object with toBase64() and fromBase64() functions
+//https://en.wikipedia.org/wiki/Base64
+String.prototype.toBase64 = function() {
+  return "";
+}
+
+String.prototype.fromBase64 = function() {
+  return ""
+}
