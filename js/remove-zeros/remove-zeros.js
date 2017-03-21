95f18ac355297b22915e82ca705d7773debdd9ca
Connie Leung <cleung334@gmail.com>
2017-03-22 00:30:18 +0800

    Do the partition without using array methods. temp object.

--- js/remove-zeros/remove-zeros.js
+++ js/remove-zeros/remove-zeros.js
@@ -10,40 +10,52 @@ function removeZeros(array) {
   
   // the correctly sorted array should be returned.
   
-  // for each element, 
-  //  if it is not zero, ignore and go to the next elements
-  //  if it is zero, visit subsequent until a non-zero eleement is found
-  //  swap both element and record the new position of the zero element
-  //  repeat the process with the next element until the end of array is reached
+  // set last zero position to array.length
+  // count number of zeros in the array list
+  // search from end of array to front
+  // if array[i] is non-zero, do nothing
+  // if array[i] is zero,  
+  //	copy array[i] to temporary variable
+  //	shift array[i+1] to array[last zero position - 1] to 1 position left
+  //	assign temporary variable to the unoccupied slot
+  //	update last zero position
+  //	decrement number of zeros to move by 1
+  //	if array[i] is not zero, repeat the process for the array[i-1]. 
+  //	Repeat the process for array[i] again
+  // terminates when i < 0 or number of zeros to move is 0
   
-  
-  let i = 0;
-  while (i < array.length) {
-    let hasSwap = false;
+  let i = array.length - 1;
+  let lastZeroPos = array.length;
+  let zeroCount = 0;  
+  for (let j = 0; j < array.length; j++) {
+    if (array[j] === '0' || array[j] === 0) {
+      zeroCount += 1;
+    }
+  }
+  while (i >= 0) {    
     if (array[i] === 0 || array[i] === '0') {
-      let zeroPos = i;
-      let j = zeroPos + 1;
-      while (j < array.length) {
-        if (array[j] !== 0 && array[j] !== '0') {
-          let temp = array[j];
-          array[j] = array[zeroPos];
-          array[zeroPos] = temp;
-          zeroPos = j;
+      const temp = array[i];  
+      let hasSwap = false;  
+      for (let j = i+1; j < lastZeroPos; j++) {
           hasSwap = true;
-        } 
-        j += 1;
+          array[j-1] = array[j];
       }
-      console.log({i: i, pass : array});
-      if (array[i] !== 0 && array[i] !== '0') { i++; }
-      else if (!hasSwap) { 
-        // no swap takes place. all zeroes are at the back of the array
+      array[lastZeroPos - 1] = temp;  
+      lastZeroPos--;
+      zeroCount--;
+      
+      if (zeroCount === 0) { 
+        // all zeros are moved to the end of the array
         break;
-      }        
+      }
+      // find the next zero
+      //if (array[i] !== 0 && array[i] !== '0') { i--; }
+      // this zero is already at the end, find the next one
+      if (!hasSwap || (array[i] !== 0 || array[i] !== '0')) { i--; }
     } else {
-      i += 1;
+      i--;
     } 
   }
-  console.log(array);
   return array;
 }
 
@@ -53,6 +65,8 @@ var input = [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14],
 
 console.log (JSON.stringify(removeZeros(input)) === JSON.stringify(solution));
 console.log (JSON.stringify(removeZeros( [0, "0", 1, 2, 3])) === JSON.stringify([1, 2, 3, 0, "0"]));
-console.log (JSON.stringify(removeZeros([ 1, null, '5', '2', 8, 6, null, 0, '0', false ])) === JSON.stringify([1,null,"5","2",8,6,null,false,"0",0]));
-console.log (JSON.stringify(removeZeros([ 1, '0', '0', 0, '0' ])) === JSON.stringify([1,"0","0","0",0]));
-console.log (JSON.stringify(removeZeros([ 1, 2, 52, 7, '3', 1, '0', 0, 0, '0' ])) === JSON.stringify([1,2,52,7,"3",1,"0",0,"0",0]));
+console.log (JSON.stringify(removeZeros([ 1, null, '5', '2', 8, 6, null, 0, '0', false ])) === JSON.stringify([1,null,"5","2",8,6,null,false,0,'0']));
+console.log (JSON.stringify(removeZeros([ 1, '0', '0', 0, '0' ])) === JSON.stringify([1,"0","0",0,'0']));
+console.log (JSON.stringify(removeZeros([ 1, 2, 52, 7, '3', 1, '0', 0, 0, '0' ])) === JSON.stringify([1,2,52,7,"3",1,"0",0,0,'0']));
+console.log (JSON.stringify(removeZeros([ 1, 0, 2, 0, 3, 0 ])) === JSON.stringify([1,2,3,0,0,0]))
+console.log (JSON.stringify(removeZeros( [ 0, 9, 0, 0, 0, 0, 0, 0, 9, 0 ])) === JSON.stringify( [ 9, 9, 0, 0, 0, 0, 0, 0, 0, 0 ]))
