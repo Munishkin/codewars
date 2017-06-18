707726f8e0c9f93a5c629ef4c6d84a628a0c9c34
Connie <cleung334@gmail.com>
2017-06-18 21:31:55 +0800

    Final solution
    use https://en.wikipedia.org/wiki/Euler's_totient_function to solve it

--- js/number-of-proper-fractions/proper-fraction.js
+++ js/number-of-proper-fractions/proper-fraction.js
@@ -15,25 +15,6 @@ Repeat steps 2,3 and 4.
 The final value of d is the required modulus of S % n. This is actually the
 way a child learns to divide two numbers. The algorithm is simple, elegant and never fails.
 */
-const reduceGCD = (s, n) => {
-      return s.split('').reduce((acc, x) => {
-          const m = 10 * acc + parseInt(x);
-          return m % n;
-      }, 0);
-}
-
-/* Binary GCD */
-const gcd = (u, v) => {
-    if (u === v || v === 0) { return u }
-    if (u === 0) { return v; }
-    if (u % 2 === 0 && v % 2 === 0) { return 2 * gcd(u / 2, v /2); }
-    else if (u % 2 === 0 && v % 2 === 1) { return gcd(u / 2, v); }
-    else if (u % 2 === 1 && v % 2 === 0) { return gcd(u, v / 2); }
-    else if (u % 2 === 1 && v % 2 === 1 && u >= v) { return gcd((u - v) / 2, v); }
-    else if (u % 2 === 1 && v % 2 === 1 && u < v) { return gcd((v - u) / 2, u); }
-    return 0;
-}
-
 // The decimal fraction of every proper fraction* is either terminating or else it is recurring.
 const findDivisors = (n) => {
   const boundary = Math.floor(Math.sqrt(n));
@@ -46,9 +27,6 @@ const findDivisors = (n) => {
       }
     }
   }
-  results.sort((a,b) => {
-    return a - b;
-  });
   return results;
 }
 
@@ -66,49 +44,33 @@ const isPrime = (n) => {
   return true
 }
 
-const properFractions = (n) => {
-  const start = new Date().getTime();
-  console.log(n);
-  if (n === 1) { return 0; }
-  if (isPrime(n)) { return n - 1; }
-  let count = 1;   // 1 / n is always reduced fraction
-  //const divisors = findDivisors(n);
-  //console.log(divisors);
-  //const isOdd = n % 2 === 1;
-  const strN = `${n}`;
-  for (let i = 2; i < n; i++) {
-    if (i % 2 === 0 && n % 2 === 0) { continue; }
-    // both even numbers are divisble by 2, not reduced fraction
-    const result = gcd(reduceGCD(strN, i), i);
-    if (result === 1) {
-      count++;
-    }
-  }
-  const end = new Date().getTime();
-  console.log(end - start);
-  return count;
+const primeDivisors = (n) => {
+    return findDivisors(n).filter((d) => {
+      return isPrime(d);
+    });
 }
 
-// try division method
+// https://en.wikipedia.org/wiki/Euler's_totient_function
+const properFractions = (n) => {
+  if (n === 1) { return 0; }
+  if (isPrime(n)) { return n - 1; }
+  // the result may be an integer, so need to find its floor
+  // formula is n * (1 - 1/p) where n is divisible by p and p is a prime number
+  return Math.floor(primeDivisors(n)
+                  .reduce((acc, d) => {
+                      return acc * (1 - 1 / d);
+                  }, n));
 
-
-//const x = reduceGCD('9999999999', 9999999997);
-//console.log(x);
-//console.log(gcd(x, 9999999997));
-
-/*console.log(properFractions(3)==2);
-console.log(properFractions(1)==0);
-console.log(properFractions(2)==1);
-console.log(properFractions(5)==4);
-console.log(properFractions(15)==8);
-console.log(properFractions(25)==20);
+}
+console.log(properFractions(3)===2);
+console.log(properFractions(1)===0);
+console.log(properFractions(2)===1);
+console.log(properFractions(5)===4);
+console.log(properFractions(15)===8);
+console.log(properFractions(25)===20);
 console.log(properFractions(500000003) === 500000002);
-console.log(properFractions(21) == 12 );
-console.log(properFractions(20) == 8 );
-console.log(properFractions(9999999));*/
-//console.log(properFractions(9999999999));
-
-//console.log(gcd(9999999999, 9999999997));
-console.log(9999999999 % 9999999997);
-console.log(9999999997 % 2);
-console.log(2 % 1);
+console.log(properFractions(21) === 12 );
+console.log(properFractions(20) === 8 );
+console.log(properFractions(9999999) === 6637344);
+console.log(properFractions(10) === 4);
+console.log(properFractions(1234567890 ) === 329040288);
