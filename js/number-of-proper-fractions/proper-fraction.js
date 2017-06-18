71af2e762374e1ae42d2ab352b447e932967bd55
Connie <cleung334@gmail.com>
2017-06-18 16:27:52 +0800

    Find gcd of large number

--- js/number-of-proper-fractions/proper-fraction.js
+++ js/number-of-proper-fractions/proper-fraction.js
@@ -1,17 +1,40 @@
-/*const gcd = (u, v) => {
+//https://souvikpal.wordpress.com/2013/02/09/gcd-of-a-very-large-number/
+// find gcd of a very large number
+/*
+So, to find the modulus, lets first store the number in a string/character
+array. Let the big number be stored in string S and normal integer be stored
+in integer n.
+
+Repeat until all characters of S are processed.
+
+Take left most character from S, convert it to corresponding integer, m.
+Take the modulus d = m % n.
+Add 10*d to the next digit i.e. the next character from left converted to int and store result in m.
+Repeat steps 2,3 and 4.
+
+The final value of d is the required modulus of S % n. This is actually the
+way a child learns to divide two numbers. The algorithm is simple, elegant and never fails.
+*/
+const reduceGCD = (s, n) => {
+      return s.split('').reduce((acc, x) => {
+          const m = 10 * acc + parseInt(x);
+          return m % n;
+      }, 0);
+}
+
+/* Binary GCD */
+const gcd = (u, v) => {
     if (u === v || v === 0) { return u }
     if (u === 0) { return v; }
-
-    if (u % 2 === 0 && v % 2 === 0) { return gcd(u / 2, v /2); }
+    if (u % 2 === 0 && v % 2 === 0) { return 2 * gcd(u / 2, v /2); }
     else if (u % 2 === 0 && v % 2 === 1) { return gcd(u / 2, v); }
     else if (u % 2 === 1 && v % 2 === 0) { return gcd(u, v / 2); }
-    else if (u % 2 === 1 && v % 2 === 1 && u > v) { return gcd((u - v) / 2, v); }
+    else if (u % 2 === 1 && v % 2 === 1 && u >= v) { return gcd((u - v) / 2, v); }
     else if (u % 2 === 1 && v % 2 === 1 && u < v) { return gcd((v - u) / 2, u); }
     return 0;
-}*/
+}
 
 // The decimal fraction of every proper fraction* is either terminating or else it is recurring.
-
 const findDivisors = (n) => {
   const boundary = Math.floor(Math.sqrt(n));
   const results = [];
@@ -44,44 +67,40 @@ const isPrime = (n) => {
 }
 
 const properFractions = (n) => {
-  console.log(n);
+  //console.log(n);
   if (n === 1) { return 0; }
   if (isPrime(n)) { return n - 1; }
   let count = 1;   // 1 / n is always reduced fraction
-  const divisors = findDivisors(n);
-  const isOdd = n % 2 === 1;
+  //const divisors = findDivisors(n);
+  //const isOdd = n % 2 === 1;
+  const strN = `${n}`;
   for (let i = 2; i < n; i++) {
     // both even numbers are divisble by 2, not reduced fraction
-    if (i % 2 === 1 || isOdd) {
-      let gcd = null;
-      for (let j = divisors.length - 1; j >= 0; j--) {
-        if (i % divisors[j] === 0) {
-          gcd = divisors[j];
-          break;
-        }
-      }
-
-      if (!gcd || gcd === 1) {  // gcd is 1, reduced fraction
-        count += 1;
-      }
+    const r = reduceGCD(strN, i);
+//    console.log({r: r});
+    const result = gcd(i, r);
+    // reduced
+    if (result === 1) {
+      //console.log({r: r, i: i});
+      count++;
     }
   }
   return count;
 }
 
-// console.log(properFractions(3)==2);
-// console.log(properFractions(1)==0);
-// console.log(properFractions(2)==1);
-// console.log(properFractions(5)==4);
-// console.log(properFractions(15)==8);
-// console.log(properFractions(25)==20);
-// console.log(properFractions(500000003) === 500000002);
-// console.log(properFractions(21) == 12 );
-// console.log(properFractions(20) == 8 );
+console.log(properFractions(3)==2);
+console.log(properFractions(1)==0);
+console.log(properFractions(2)==1);
+console.log(properFractions(5)==4);
+console.log(properFractions(15)==8);
+console.log(properFractions(25)==20);
+console.log(properFractions(500000003) === 500000002);
+console.log(properFractions(21) == 12 );
+console.log(properFractions(20) == 8 );
+
 
 console.log(properFractions(9999999));
-
-//console.log(properFractions(9999999999));
+console.log(properFractions(9999999999));
 
 
 //console.log(findDivisors(9999999));
