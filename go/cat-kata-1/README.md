ba2847ee81a67ca4f919626e2d0699d6c74805d5
Connie Leung <cleung334@gmail.com>
2017-12-09 23:30:58 +0800

    skeleton code

---
+++ go/cat-kata-1/README.md
@@ -0,0 +1,3 @@
+Cat Kata Part 1
+
+https://www.codewars.com/kata/cat-kata-part-1/train/go
---
+++ go/cat-kata-1/cat-kata-1.go
@@ -0,0 +1,6 @@
+package kata
+
+func PeacefulYard(yard []string, minDistance int) bool {
+	// your code here
+  return false
+}
